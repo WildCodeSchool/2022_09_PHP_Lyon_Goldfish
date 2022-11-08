0d84a104f44771bca0295601c55968130ac797ef
JonasJallet <jonas.jallet@gmail.com>
2022-11-09 00:06:50 +0100

    update navbar with active menu, fix troubles, update css & bootstrap

---
+++ public/assets/js/navbar-active.js
@@ -0,0 +1,23 @@
+switch (!0) {
+    case /artists/.test(window.location.pathname):
+        document.getElementById("artists-desktop-text").classList.add("active");
+        document.getElementById("artists-desktop-img").classList.add("active");
+        document.getElementById("artists-mobile-text").classList.add("active");
+        document.getElementById("artists-mobile-img").classList.add("active");
+        break;
+    case /concerts/.test(window.location.pathname):
+        document.getElementById("concerts-desktop-text").classList.add("active");
+        document.getElementById("concerts-desktop-img").classList.add("active");
+        document.getElementById("concerts-mobile-text").classList.add("active");
+        document.getElementById("concerts-mobile-img").classList.add("active");
+        break;
+    case /aged/.test(window.location.pathname):
+        document.getElementById("venues-text").classList.add("active");
+        document.getElementById("venues-img").classList.add("active");
+        break;
+    default:
+        document.getElementById("home-desktop-text").classList.add("active");
+        document.getElementById("home-desktop-img").classList.add("active");
+        document.getElementById("home-mobile-text").classList.add("active");
+        document.getElementById("home-mobile-img").classList.add("active");
+}
