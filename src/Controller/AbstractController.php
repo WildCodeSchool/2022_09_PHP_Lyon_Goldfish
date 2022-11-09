d44a7aabde5a87d7522296e82999f9bc9a717263
JonasJallet <jonas.jallet@gmail.com>
2022-11-09 11:45:46 +0100

    add sessions

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -2,28 +2,27 @@
 
 namespace App\Controller;
 
+use App\Model\UserManager;
 use Twig\Environment;
 use Twig\Extension\DebugExtension;
 use Twig\Loader\FilesystemLoader;
 
-/**
- * Initialized some Controller common features (Twig...)
- */
 abstract class AbstractController
 {
     protected Environment $twig;
-
-
+    protected array|false $user;
+    /**
+     *  Initializes this class.
+     */
     public function __construct()
     {
         $loader = new FilesystemLoader(APP_VIEW_PATH);
         $this->twig = new Environment(
-            $loader,
-            [
-                'cache' => false,
-                'debug' => true,
-            ]
+            $loader
         );
         $this->twig->addExtension(new DebugExtension());
+        $userManager = new UserManager();
+        $this->user = isset($_SESSION['user_id']) ? $userManager->selectOneById($_SESSION['user_id']) : false;
+        $this->twig->addGlobal('user', $this->user);
     }
 }
--- src/Model/UserManager.php
+++ src/Model/UserManager.php
@@ -2,8 +2,15 @@
 
 namespace App\Model;
 
-use PDO;
-
 class UserManager extends AbstractManager
 {
+    public const TABLE = 'user';
+    public function selectOneByEmail(string $email): array|false
+    {
+        // prepared request
+        $statement = $this->pdo->prepare("SELECT * FROM " . static::TABLE . " WHERE email=:email");
+        $statement->bindValue(':email', $email, \PDO::PARAM_STR);
+        $statement->execute();
+        return $statement->fetch();
+    }
 }
