aaac1f2f5166b12a45ad00b5249aea91b7bafbc7
JonasJallet <jonas.jallet@gmail.com>
2022-11-21 20:20:44 +0100

    delete all elements about js script for agenda

--- public/assets/js/agenda-plus.js
+++
@@ -1,5 +0,0 @@
-function toggle() {
-    let visibleArray = (document).getElementsByClassName("visible");
-    for (let i = 0; i < visibleArray.length; i++)
-        visibleArray.item(i).classList.toggle('d-none');
-}
