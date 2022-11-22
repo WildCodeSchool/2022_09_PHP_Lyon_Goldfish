2b6089d317d28a568fca5ce81d54d9c52a69886b
JonasJallet <jonas.jallet@gmail.com>
2022-11-22 15:14:46 +0100

    update name road for link delete

--- src/routes.php
+++ src/routes.php
@@ -21,8 +21,8 @@ return [
     'concerts/favorites' => ['FavoriteController', 'favoritesConcerts',],
     'concerts/myArtists' => ['FavoriteController', 'concertsForMyArtists',],
     'concerts/deleteFavorite' => ['FavoriteController', 'deleteFavoriteConcert',],
-    'concerts/deleteFavorite2' => ['FavoriteController', 'deleteFavoriteConcertMyArtists',],
-    'concerts/deleteFavorite3' => ['FavoriteController', 'deleteMyFavoriteConcert',],
+    'concerts/deleteFavoriteInArtists' => ['FavoriteController', 'deleteFavoriteConcertMyArtists',],
+    'concerts/deleteFavoriteInFavorites' => ['FavoriteController', 'deleteMyFavoriteConcert',],
 
     'artists' => ['ArtistController', 'index',],
     'artists/edit' => ['ArtistController', 'edit', ['id']],
@@ -32,7 +32,7 @@ return [
     'artists/search' => ['ArtistController', 'search',],
     'artists/favorites' => ['FavoriteController', 'favoritesArtists',],
     'artists/deleteFavorite' => ['FavoriteController', 'deleteFavoriteArtist',],
-    'artists/deleteFavorite2' => ['FavoriteController', 'deleteMyFavoriteArtist',],
+    'artists/deleteFavoriteInFavorites' => ['FavoriteController', 'deleteMyFavoriteArtist',],
 
     'venues' => ['VenueController', 'index',],
     'venues/edit' => ['VenueController', 'edit', ['id']],
