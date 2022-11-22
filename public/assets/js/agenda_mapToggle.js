03c20f3fd9977ffe86dbbac96ecfa1bd5fb157b0
JonasJallet <jonas.jallet@gmail.com>
2022-11-22 14:52:48 +0100

    update without console log on js files

--- public/assets/js/agenda_mapToggle.js
+++ public/assets/js/agenda_mapToggle.js
@@ -4,14 +4,9 @@ for (let i = 0; i < classCards.length; i++) {
     classCards[i].addEventListener("click", function() 
     {
     let id = this.id.substr(5);
-    console.log(id);  
-
     let mapId = "map_" + id;
-
     let map = document.getElementById(mapId);
-
     map.classList.toggle("d-none");
-    console.log(map);
     })
 }
 
